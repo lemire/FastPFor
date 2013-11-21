698429489c88cf65b3d251843999620cf7e40459
Daniel Lemire <lemire@gmail.com>
2013-11-21 09:28:37 -0500

    Fighting off the evil tabs

--- example.cpp
+++ example.cpp
@@ -1,7 +1,7 @@
 //
 // A simple example to get you started with the library.
 // You can compile and run this example like so:
-//  
+//
 //   make example
 //   ./example
 //
@@ -14,31 +14,31 @@
 
 
 int main() {
-	// We pick a CODEC
-	IntegerCODEC & codec =  * CODECFactory::getFromName("simdfastpfor");
-	// could use others, e.g., "simdbinarypacking", "varintg8iu"
-	////////////
-	//
-	// create a container with some integers in it
-	//
-	// for this example, we will not assume that the
-	// integers are in sorted order
-	//
-	// (Note: You don't need to use a vector.) 
-	// 
-	size_t N = 10 * 1000;
-	vector<uint32_t> mydata(N);
-	for(uint32_t i = 0; i < N;i += 150) mydata[i] = i;
-	//
-	// the vector mydata could contain anything, really
-	// 
-	///////////
-	// 
-	// You need some "output" container. You are responsible
-	// for allocating enough memory.
-	//
-	vector<uint32_t> compressed_output(N+1024);
-	// N+1024 should be plenty
+    // We pick a CODEC
+    IntegerCODEC & codec =  * CODECFactory::getFromName("simdfastpfor");
+    // could use others, e.g., "simdbinarypacking", "varintg8iu"
+    ////////////
+    //
+    // create a container with some integers in it
+    //
+    // for this example, we will not assume that the
+    // integers are in sorted order
+    //
+    // (Note: You don't need to use a vector.)
+    //
+    size_t N = 10 * 1000;
+    vector<uint32_t> mydata(N);
+    for(uint32_t i = 0; i < N;i += 150) mydata[i] = i;
+    //
+    // the vector mydata could contain anything, really
+    //
+    ///////////
+    //
+    // You need some "output" container. You are responsible
+    // for allocating enough memory.
+    //
+    vector<uint32_t> compressed_output(N+1024);
+    // N+1024 should be plenty
     //
     //
     size_t compressedsize = compressed_output.size();
@@ -50,12 +50,12 @@ int main() {
     compressed_output.shrink_to_fit();
     // display compression rate:
     cout<<setprecision(3);
-    cout<<"You are using " << 32.0 * static_cast<double>(compressed_output.size()) / 
+    cout<<"You are using " << 32.0 * static_cast<double>(compressed_output.size()) /
                  static_cast<double>(mydata.size()) <<" bits per integer. "<<endl;
     //
     // You are done!... with the compression...
     //
-    /// 
+    ///
     // decompressing is also easy:
     //
     vector<uint32_t> mydataback(N);
@@ -68,7 +68,7 @@ int main() {
     // That's it!
     //
     if(mydataback != mydata) throw runtime_error("bug!");
-    
+
     // If you need to use differential coding, you can use
     // calls like these to get the deltas and recover the original
     // data from the deltas:
