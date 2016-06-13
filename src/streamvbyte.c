48035758c387ec6b10a826df0d18d51becb7b382
Daniel Lemire <lemire@gmail.com>
2016-06-13 12:00:50 -0400

    More portable includes.

--- src/streamvbyte.c
+++ src/streamvbyte.c
@@ -5,9 +5,26 @@
 // each 8-bit key has four 2-bit lengths: 00=1B, 01=2B, 10=3B, 11=4B
 // no particular alignment is assumed or guaranteed for any elements
 
-#ifndef _MSC_VER
-#include <x86intrin.h>
+#if defined(_MSC_VER)
+     /* Microsoft C/C++-compatible compiler */
+     #include <intrin.h>
+#elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
+     /* GCC-compatible compiler, targeting x86/x86-64 */
+     #include <x86intrin.h>
+#elif defined(__GNUC__) && defined(__ARM_NEON__)
+     /* GCC-compatible compiler, targeting ARM with NEON */
+     #include <arm_neon.h>
+#elif defined(__GNUC__) && defined(__IWMMXT__)
+     /* GCC-compatible compiler, targeting ARM with WMMX */
+     #include <mmintrin.h>
+#elif (defined(__GNUC__) || defined(__xlC__)) && (defined(__VEC__) || defined(__ALTIVEC__))
+     /* XLC or GCC-compatible compiler, targeting PowerPC with VMX/VSX */
+     #include <altivec.h>
+#elif defined(__GNUC__) && defined(__SPE__)
+     /* GCC-compatible compiler, targeting PowerPC with SPE */
+     #include <spe.h>
 #endif
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
