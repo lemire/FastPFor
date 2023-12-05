735dc039213265426083b514d67ff62fb482242a
kosakseb <sebastian.kosak@tum.de>
2023-12-05 11:59:47 +0100

    removed arm_neon and added arch64

--- headers/util.h
+++ headers/util.h
@@ -175,7 +175,7 @@ inline void checkifdivisibleby(size_t a, uint32_t x) {
 __attribute__((const)) inline uint32_t asmbits(const uint32_t v) {
 #ifdef _MSC_VER
   return gccbits(v);
-#elifdef __arch64__
+#elif defined(__arch64__)
   return gccbits(v);
 #else
   if (v == 0)
