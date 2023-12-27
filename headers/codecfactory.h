85eabae62473491eb3516ae3b10ce2bf79991e02
kosakseb <sebastian.kosak@tum.de>
2023-12-27 14:49:10 +0100

    removed unnecessary parts

--- headers/codecfactory.h
+++ headers/codecfactory.h
@@ -10,36 +10,6 @@
 
 #include "codecs.h"
 
-#include "common.h"
-#include "codecs.h"
-#include "vsencoding.h"
-#include "util.h"
-#include "simple16.h"
-#include "simple9.h"
-#include "simple9_rle.h"
-#include "simple8b.h"
-#include "simple8b_rle.h"
-#include "newpfor.h"
-#include "simdnewpfor.h"
-#include "optpfor.h"
-#include "simdoptpfor.h"
-#include "fastpfor.h"
-#include "simdfastpfor.h"
-#include "variablebyte.h"
-#include "compositecodec.h"
-#include "blockpacking.h"
-#include "pfor.h"
-#include "simdpfor.h"
-#include "pfor2008.h"
-#include "VarIntG8IU.h"
-#include "simdbinarypacking.h"
-#include "snappydelta.h"
-#include "varintgb.h"
-#include "simdvariablebyte.h"
-#include "streamvariablebyte.h"
-#include "simdgroupsimple.h"
-
-
 namespace FastPForLib {
 
 typedef std::map<std::string, std::shared_ptr<IntegerCODEC>> CodecMap;
